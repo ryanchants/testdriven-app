6db4b9e58741172bef16622a62ba85804cf3d675
bhchance <bhchance@gmail.com>
2020-02-01 09:18:41 -0600

    Part 2: Adding Travis CI support

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -8,20 +8,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
 
     # set config
-    app_settings = os.getenv('APP_SETTINGS')
+    app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
     db.init_app(app)
 
     from project.api.users import users_blueprint
+
     app.register_blueprint(users_blueprint)
 
     @app.shell_context_processor
     def ctx():
-        return {"app":app, "db": db}
+        return {"app": app, "db": db}
+
     return app
